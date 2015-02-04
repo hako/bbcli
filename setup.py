2d7f1500503a70613e9c23178d1fa46a1b66f810
Wesley Hill <hako@users.noreply.github.com>
2015-02-04 11:11:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='bbcli',
-    version='0.1.4',
+    version='0.1.5',
     description='Browse BBC News through the command line (based on pyhackernews)',
     long_description=open('README.rst').read(),
     license=open('LICENSE').read(),
