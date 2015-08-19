458f6379c90018366122fa5c2b507bebdebd6e78
Wesley Hill <wesley@hakobaito.co.uk>
2015-08-19 12:18:18 +0100

    import arrow.
    note to self: use virtualenv next time.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='bbcli',
-    version='0.2',
+    version='0.2.1',
     description='Browse BBC News like a hacker. (based on pyhackernews)',
     long_description=open('README.rst').read(),
     license=open('LICENSE').read(),
@@ -12,7 +12,7 @@ setup(
     author_email='wesley@hakobaito.co.uk',
     url='https://github.com/hako/bbcli',
     packages=['bbcli'],
-    install_requires=['urwid', 'requests'],
+    install_requires=['urwid', 'requests', 'arrow'],
     classifiers=[
         'Environment :: Console',
         'License :: OSI Approved :: MIT License',
